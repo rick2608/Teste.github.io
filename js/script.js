5878abda359395b186bc50985e6831e81aed81d2
Mendeshxzz <richardmendes1606@gmail.com>
2026-08-14 11:15:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 // as const btn e status são variáveis constantes
 
-const btn = document.getElementById('btn-gravador');
+// const btn = document.getElementById('btn-gravador');
 
-const status = document.getElementById('status-gravacao');
-
-
-
-// quando o botão for clicado, ele muda a cor e o texto
-
-btn.addEventListener('mousedown', () => {
-
-    // muda a cor do botão para vermelho e o texto para "Gravando..."
-
-    btn.style.backgroundColor = '#e74c3c';
-
-    // muda o texto do botão para "Gravando... Não solte!" e o status para "Capturando áudio..."
-
-    btn.innerText = '🔴 Gravando... Não solte!';
-
-    // muda o texto do status para "Capturando áudio..."
-
-    status.innerText = 'Status: Capturando áudio...';
-
-});
+// const status = document.getElementById('status-gravacao');
 
 
 
-// quando o botão for solto, ele muda a cor e o texto
+// // quando o botão for clicado, ele muda a cor e o texto
 
-btn.addEventListener('mouseup', () => {
+// btn.addEventListener('mousedown', () => {
 
-    // muda a cor do botão para azul e o texto para "Clique e Segure para Gravar"
+//     // muda a cor do botão para vermelho e o texto para "Gravando..."
 
-    btn.style.backgroundColor = '#3498db';
+//     btn.style.backgroundColor = '#e74c3c';
 
-    // muda o texto do botão para "Clique e Segure para Gravar" e o status para "Gravação concluída e enviada!"
+//     // muda o texto do botão para "Gravando... Não solte!" e o status para "Capturando áudio..."
 
-    btn.innerText = '🎤 Clique e Segure para Gravar';
+//     btn.innerText = '🔴 Gravando... Não solte!';
 
-    // muda o texto do status para "Gravação concluída e enviada!"
+//     // muda o texto do status para "Capturando áudio..."
 
-    status.innerText = 'Status: Gravação concluída e enviada!';
+//     status.innerText = 'Status: Capturando áudio...';
 
-});
+// });
+
+
+
+// // quando o botão for solto, ele muda a cor e o texto
+
+// btn.addEventListener('mouseup', () => {
+
+//     // muda a cor do botão para azul e o texto para "Clique e Segure para Gravar"
+
+//     btn.style.backgroundColor = '#3498db';
+
+//     // muda o texto do botão para "Clique e Segure para Gravar" e o status para "Gravação concluída e enviada!"
+
+//     btn.innerText = '🎤 Clique e Segure para Gravar';
+
+//     // muda o texto do status para "Gravação concluída e enviada!"
+
+//     status.innerText = 'Status: Gravação concluída e enviada!';
+
+// });
 
 
 
